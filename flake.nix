96cd69bda7a028b626e7861a279c76fec734b2b5
Aner Lucero <4rgento@gmail.com>
2023-02-24 00:56:21 -0300

    Initial Commit

---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "A development environment to run typescript in neovim";
+
+  inputs = {
+    nvim-vimrc-code.url = "github:argent0/flake-nvim-vimrc-code";
+  };
+
+  outputs = { self, nixpkgs, nvim-vimrc-code }: 
+  let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in {
+
+    packages.x86_64-linux.default = pkgs.stdenv.mkDerivation {
+      name = "flake-nvim-typescript";
+      src = ./.;
+      buildInputs = with pkgs; [
+        nvim-vimrc-code.packages.x86_64-linux.default
+      ];
+      installPhase = ''
+        mkdir -p $out/etc/nvim
+        cat \
+          ${nvim-vimrc-code.packages.x86_64-linux.default.outPath}/etc/nvim/vimrc \
+          ${./lua-start} \
+          ${./treesitter.lua} \
+          ${./lspconfig.lua} \
+          ${./lua-end}  > $out/etc/nvim/vimrc
+      '';
+    };
+
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      buildInputs = let
+        vimrcPath = "${self.packages.x86_64-linux.default}/etc/nvim/vimrc";
+        local-neovim = pkgs.neovim.override {
+          configure = {
+              # Additional plugins to be installed
+              packages.myVimPackages = with pkgs.vimPlugins; {
+                start = [
+                  vim-nix
+                  copilot-vim
+                  vim-surround
+                  nvim-lspconfig
+                  nvim-cmp
+                  cmp-nvim-lsp
+                  (nvim-treesitter.withPlugins (p: with p; [ typescript ]))
+                ];
+                opt = [ ];
+              };
+              customRC = builtins.readFile vimrcPath;
+            };
+          };
+      in [
+        pkgs.nodePackages.typescript
+        pkgs.nodePackages.typescript-language-server
+        pkgs.nodejs
+        local-neovim
+      ];
+    };
+  };
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+# flake-nvim-typescript
+
+A development environment to develop with typescript in neovim
+
+## Develop
+
+```
+nix develop
+```
